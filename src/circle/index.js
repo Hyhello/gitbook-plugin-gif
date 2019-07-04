bf739f608a542b2647975d0cc302ebc74177414e
Hyhello <673089899@qq.com>
2019-07-05 06:37:53 +0800

    feat: 初始化项目

---
+++ src/circle/index.js
@@ -0,0 +1,17 @@
+/**
+ * 作者：yeshengqiang
+ * 时间：2019-06-23
+ * 描述：环形进度条
+ */
+
+import render from './render';
+
+const Circle = options => {
+	this.$conf = options;
+	// 开始绘制
+	this.render();
+};
+
+render(Circle);
+
+export default Circle;
